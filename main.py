91b665ac83616e326f329b1d9605ff5e54cddc68
sadra goudarzdashti <s.goudarzdashti@gmail.com>
2021-07-12 14:39:48 +0430

    fix some bugs / add new features

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ no_classes = 0                       # Number of classes
 no_class_public_attribute = dict()   # Number of each class(package.class) public attribute
 no_class_private_attribute = dict()  # Number of each class(package.class) private attribute
 no_class_protected_attribute = dict()  # Number of each class protected(package.class) attribute
-class_package = dict()               # key: class_name, value: package_name
+class_package = list()               # list of (class_name, package_name)
+package_class_method = list()        # list of (package_name, class_name, method_name)
 classes_access = dict()              # key: (package, class), value: accessed class(package, class)
+methods_access = dict()              # key: (package, class, method), value: accessed class(package, class, method)
 
 
 class FirstListener(JavaParserLabeledListener):
@@ -26,7 +28,7 @@ class FirstListener(JavaParserLabeledListener):
         global no_classes
         no_classes += 1
         self.class_name = ctx.IDENTIFIER().getText()
-        class_package[self.class_name] = self.package_name
+        class_package.append((self.class_name, self.package_name))
         no_class_public_attribute[(self.package_name, self.class_name)] = 0
         no_class_private_attribute[(self.package_name, self.class_name)] = 0
         no_class_protected_attribute[(self.package_name, self.class_name)] = 0
@@ -50,31 +52,50 @@ class SecondListener(JavaParserLabeledListener):
         self.class_name = str()
         self.package_name = ''
         self.imported = set()
+        self.method_name = ''
+        self.instances_in_method = dict()  # key:instance_name value:(package, class)
 
     def enterPackageDeclaration(self, ctx: JavaParserLabeled.PackageDeclarationContext):
         self.package_name = ctx.qualifiedName().getText()
 
     def enterClassDeclaration(self, ctx: JavaParserLabeled.ClassDeclarationContext):
         self.class_name = ctx.IDENTIFIER().getText()
-        classes_access[(self.package_name, self.class_name)] = {}
+        classes_access[(self.package_name, self.class_name)] = set()
 
     def enterImportDeclaration(self, ctx: JavaParserLabeled.ImportDeclarationContext):
         self.imported.add(ctx.qualifiedName().getText())
+
+    def enterMethodDeclaration(self, ctx:JavaParserLabeled.MethodDeclarationContext):
+        self.method_name = ctx.IDENTIFIER().getText()
+        package_class_method.append((self.package_name, self.class_name, self.method_name))
 
     def exitLocalVariableDeclaration(self, ctx: JavaParserLabeled.LocalVariableDeclarationContext):
         try:
             instance_class = ctx.variableDeclarators().variableDeclarator(0) \
                 .variableInitializer().expression().creator() \
                 .createdName().getText()
-            for source_class, source_package in class_package.items():
+            for source_class, source_package in class_package:
                 if ((self.package_name == source_package or source_package in self.imported) \
                     and instance_class == source_class) or \
                         (instance_class == source_package + '.' + source_class):
 
-                    classes_access[(self.package_name, self.class_name)] = {(source_package, source_class)}
+                    classes_access[(self.package_name, self.class_name)].add((source_package, source_class))
+                    instance_name = ctx.variableDeclarators().variableDeclarator(0).variableDeclaratorId().getText()
+                    self.instances_in_method[instance_name] = (source_package, source_class)
+                    methods_access[(self.package_name, self.class_name, self.method_name)] = set()
         except:
             pass
 
+    def enterMethodCall0(self, ctx:JavaParserLabeled.MethodCall0Context):
+        method_name = ctx.IDENTIFIER().getText()
+        obj_name = ctx.parentCtx.expression().getText()
+        if obj_name in self.instances_in_method:
+            methods_access[(self.package_name, self.class_name, self.method_name)].add(
+                (self.instances_in_method[obj_name][0], self.instances_in_method[obj_name][1], method_name))
+
+    def exitMethodDeclaration(self, ctx:JavaParserLabeled.MethodDeclarationContext):
+        self.method_name = ''
+        self.instances_in_method = {}
 
 def main():
     for root, dirs, files in os.walk('java'):
@@ -102,12 +123,6 @@ def main():
                 walker.walk(t=tree, listener=listener)
 
     global no_classes
-    # print(no_classes)
-    # print(no_class_public_attribute)
-    # print(no_class_private_attribute)
-    # print(no_class_protected_attribute)
-    # print(classes_access)
-    # print(class_package)
     print('---------------------------- NUMBER OF CLASSES : ', no_classes, ' ---------------------------')
     print("---------------------------- NUMBER OF ATTRIBUTES ------------------------------")
     print("++++++ PUBLIC ATTRS ++++++")
@@ -123,7 +138,7 @@ def main():
         print(package_and_class_name[0] + '.' + package_and_class_name[1], ':', no)
 
     print("\n------------------------ NUMBER OF CLASSES ACCESSED BY EACH CLASS --------------------------")
-    for class_name, package_name in class_package.items():
+    for class_name, package_name in class_package:
         i = 0
         for _, pc in classes_access.items():
             if (package_name, class_name) in pc:
@@ -134,6 +149,17 @@ def main():
     for class_and_package_name, accessed in classes_access.items():
         print(class_and_package_name[0] + '.' + class_and_package_name[1], ':', len(accessed))
 
+    print("\n------------------------ NUMBER OF METHODS ACCESSED BY EACH METHOD --------------------------")
+    for package_name, class_name, method_name in package_class_method:
+        i = 0
+        for _, pcm in methods_access.items():
+            if (package_name, class_name, method_name) in pcm:
+                i += 1
+        print(package_name + '.' + class_name + '.' + method_name, 'accessed by ', i, ' methods.')
+
+    print("\n------------------------ NUMBER OF METHODS EACH METHOD ACCESS --------------------------")
+    for pcm, accessed in methods_access.items():
+        print(pcm[0] + '.' + pcm[1] + '.' + pcm[2], ':', len(accessed))
 
 
 if __name__ == '__main__':
